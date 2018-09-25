8b5e274f639afbf73e17cfe2cc63920da131add0
Tanvir Hasan Piash <piash1465@gmail.com>
2018-09-25 17:51:18 +0600

    missing files added

---
+++ WeDo/Controllers/Service Provider/SPProfileViewController.temp_caseinsensitive_rename.swift
@@ -0,0 +1,41 @@
+//
+//  SPProfileViewController.swift
+//  WeDo
+//
+//  Created by Tanvir Hasan Piash on 20/9/18.
+//  Copyright © 2018 Creativeitem. All rights reserved.
+//
+
+import UIKit
+
+class SPProfileViewController: UIViewController {
+    
+    let backgroundImageView : UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .clear
+        imageView.image = #imageLiteral(resourceName: "bigger-logo")
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        navigationController?.navigationBar.barTintColor = NAVBAR_BG_COLOR
+        let logo = UIImage(named: "logo.png")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
+        //navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "back-icon"), style: .plain, target: self, action: #selector(backTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "menu"), style: .plain, target: self, action: #selector(menuIconTapped))
+    }
+    
+    @objc func menuIconTapped() {
+        self.menu.show(self)
+    }
+    
+}
